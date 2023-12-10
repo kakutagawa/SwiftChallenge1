18c9c9f6c99529a96e03c0d9f40018cb56341bb9
kakutagawa <kaktgw0824@gmail.com>
2023-12-10 12:38:40 +0900

    Initial Commit

---
+++ SwiftChallenge1/SwiftChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallenge1App.swift
+//  SwiftChallenge1
+//
+//  Created by 芥川浩平 on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
