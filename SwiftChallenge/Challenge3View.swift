b2ceb65a3609c7f9f3817d1137c9e1e902254944
kakutagawa <kaktgw0824@gmail.com>
2023-12-20 16:59:12 +0900

    Challenge4

--- SwiftChallenge/Challenge3View.swift
+++ SwiftChallenge/Challenge3View.swift
@@ -5,40 +5,33 @@
 //  Created by 芥川浩平 on 2023/12/13.
 //
 
-import SwiftUI
-
-struct Challenge3Profile: Identifiable {
-    let id = UUID()
-    let name: String
-    let age: Int
-}
-
-struct Challenge3View: View {
-    var users: [Challenge3Profile] = []
-
-    init() {
-        var userList = [Challenge3Profile]()
-        for i in 1...100 {
-            let user = Challenge3Profile(name: "ユーザー\(i)", age: i)
-            userList.append(user)
-        }
-        self.users = userList
-    }
-    var body: some View {
-        ScrollView {
-            ForEach(users) { user in
-                HStack {
-                    Text("名前：\(user.name)")
-                    Spacer()
-                    Text("年齢：\(user.age)歳")
-                }
-                .frame(height: 60)
-                .padding(.horizontal, 32)
-            }
-        }
-    }
-}
-
-#Preview {
-    Challenge3View()
-}
+//import SwiftUI
+//
+//struct Challenge3Profile: Identifiable {
+//    let id = UUID()
+//    let name: String
+//    let age: Int
+//}
+//
+//struct Challenge3View: View {
+////    private let users: [Challenge3Profile] = (1...100).map{ $0 }
+////    let user = Challenge3Profile(name: "ユーザー\(i), age: i)
+//
+//    var body: some View {
+//        ScrollView {
+//            ForEach(users) { user in
+//                HStack {
+//                    Text("名前：\(user.name)")
+//                    Spacer()
+//                    Text("年齢：\(user.age)歳")
+//                }
+//                .frame(height: 60)
+//                .padding(.horizontal, 32)
+//            }
+//        }
+//    }
+//}
+//
+//#Preview {
+//    Challenge3View()
+//}
---
+++ SwiftChallenge/Challenge4View.swift
@@ -0,0 +1,57 @@
+//
+//  Challenge4View.swift
+//  SwiftChallenge
+//
+//  Created by 芥川浩平 on 2023/12/19.
+//
+
+import SwiftUI
+
+//enum Icons: String, CaseIterable {
+//    case mic.fill
+//    case message.fill
+//    case bubble.fill
+//    case phone.fill
+//    case video.fill
+//}
+
+struct Challenge4View: View {
+    @State private var selectedIcon: String? = "mic.fill"
+    @State private var selectedColor: Color? = .green
+
+    var body: some View {
+        let iconOptions = ["mic.fill", "message.fill", "bubble.fill", "phone.fill", "video.fill"]
+        let colorOptions: [Color] = [.red, .blue, .yellow, .green, .orange, .purple]
+
+        VStack(spacing: 48) {
+            Picker("Icon", selection: $selectedIcon) {
+                ForEach(iconOptions, id: \.self) { icon in
+                    Image(systemName: icon).tag(icon)
+                }
+            }
+            .pickerStyle(.segmented)
+            HStack(spacing: 24) {
+                ForEach(colorOptions, id: \.self) { color in
+                    Button {
+                        self.selectedColor = color
+                    } label : {
+                        Circle()
+                            .fill(color)
+                            .frame(width: 28, height: 28)
+                    }
+                }
+            }
+            if let selectedIcon = selectedIcon, let selectedColor = selectedColor {
+                Image(systemName: selectedIcon)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48)
+                    .foregroundStyle(selectedColor)
+            }
+        }
+    }
+}
+
+#Preview {
+    Challenge4View()
+}
