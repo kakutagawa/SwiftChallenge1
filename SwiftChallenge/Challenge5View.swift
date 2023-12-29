16a1d5435a15b14a94efd91c5472ee692ddda379
kakutagawa <kaktgw0824@gmail.com>
2023-12-29 16:44:14 +0900

    Challenge6

--- SwiftChallenge/Challenge5View.swift
+++ SwiftChallenge/Challenge5View.swift
@@ -61,6 +61,7 @@ struct Challenge5View: View {
                             .frame(height: 80)
                             Divider()
                         }
+                        .foregroundStyle(.black)
                     }
                     .padding(.horizontal, 16)
                 }
@@ -71,6 +72,22 @@ struct Challenge5View: View {
                 await apiClient.request()
             }
         }
+    }
+}
+
+struct ToDoDetailView: View {
+    let todo: Todo
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("ユーザーID: \(todo.userId)")
+                .font(.caption)
+            Text("タイトル: \(todo.title)")
+                .font(.headline)
+
+        }
+        .padding(16)
+        .navigationTitle(todo.completed ? "完了タスク" : "非完了タスク")
     }
 }
 
--- SwiftChallenge/ToDoDetailView.swift
+++ SwiftChallenge/ToDoDetailView.swift
@@ -5,24 +5,10 @@
 //  Created by 芥川浩平 on 2023/12/28.
 //
 
-import SwiftUI
-
-struct ToDoDetailView: View {
-    let todo: Todo
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("ユーザーID: \(todo.userId)")
-                .font(.caption)
-            Text("タイトル: \(todo.title)")
-                .font(.headline)
-
-        }
-        .padding(16)
-        .navigationTitle(todo.completed ? "完了タスク" : "非完了タスク")
-    }
-}
-
-#Preview {
-    ToDoDetailView()
-}
+//import SwiftUI
+//
+//
+//
+//#Preview {
+//    ToDoDetailView(todo: )
+//}
